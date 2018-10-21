d63a0b5db35a9b250e6ec1695cc4c5a5dfb9742a
Lateef Koshemani <drlatt@gmail.com>
2018-10-21 08:59:51 +0100

    creating ELB

--- java_app_deploy/main.tf
+++ java_app_deploy/main.tf
@@ -1,16 +1,36 @@
-# resource "aws_elb" "load-balancer" {
-#   name = "load-balancer"
+# resource "aws_elb" "javaapp" {
+#   name                      = "javaapp"
+#   availability_zones        = ["eu-west-1a, eu-west-1b, eu-west-1c"]
+#   cross_zone_load_balancing = true
+#   subnets = ["${aws_subnet.subnet-1.id}", "${aws_subnet.subnet-2.id}"]
+#   security_groups = ["${aws_security_group.secgroup-elb.id}"]
 
 #   listener {
-#     lb_port = 80
-#     instance_port = 80
-
-#     lb_protocol = "http"
-#     instance_protocol = "http"
+#     instance_port      = 8080
+#     instance_protocol  = "http"
+#     lb_port            = 443
+#     lb_protocol        = "https"
+#     # ssl_certificate_id = "${aws_iam_server_certificate.test_cert.arn}"
 #   }
 
-#   instances = ["${aws_instance.web.*.id}"]
+#   health_check = [
+#     {
+#       target              = "HTTP:8080/"
+#       interval            = 30
+#       healthy_threshold   = 2
+#       unhealthy_threshold = 2
+#       timeout             = 5
+#     },
+#   ]
+
+# #   instances = ["${aws_instance.web.*.id}"]
+
+#   tags = {
+#       Name = "javaapp"
+#   }
 # }
+
+
 
 # resource "aws_instance" "web" {
 
