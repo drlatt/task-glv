89d41e92c3879532a071a04c1ef1da3eac80288d
Lateef Koshemani <drlatt@gmail.com>
2018-10-21 12:17:26 +0100

    temporarily changed ELB port to 80

--- java_app_deploy/main.tf
+++ java_app_deploy/main.tf
@@ -1,34 +1,36 @@
-# resource "aws_elb" "javaapp" {
-#   name                      = "javaapp"
-#   availability_zones        = ["eu-west-1a, eu-west-1b, eu-west-1c"]
-#   cross_zone_load_balancing = true
-#   subnets = ["${aws_subnet.subnet-1.id}", "${aws_subnet.subnet-2.id}"]
-#   security_groups = ["${aws_security_group.secgroup-elb.id}"]
+resource "aws_elb" "javaapp" {
+  name                      = "javaapp"
+#   availability_zones        = ["eu-west-1"]
+  cross_zone_load_balancing = true
+  subnets = ["${aws_subnet.subnet-1.id}", "${aws_subnet.subnet-2.id}"]
+  security_groups = ["${aws_security_group.secgroup-elb.id}"]
 
-#   listener {
-#     instance_port      = 8080
-#     instance_protocol  = "http"
-#     lb_port            = 443
-#     lb_protocol        = "https"
-#     # ssl_certificate_id = "${aws_iam_server_certificate.test_cert.arn}"
-#   }
+  listener {
+    instance_port      = 8080
+    instance_protocol  = "http"
+    lb_port = 80
+    lb_protocol = "http"
+    # lb_port            = 443
+    # lb_protocol        = "https"
+    # ssl_certificate_id = "${aws_iam_server_certificate.test_cert.arn}"
+  }
 
-#   health_check = [
-#     {
-#       target              = "HTTP:8080/"
-#       interval            = 30
-#       healthy_threshold   = 2
-#       unhealthy_threshold = 2
-#       timeout             = 5
-#     },
-#   ]
+  health_check = [
+    {
+      target              = "HTTP:8080/"
+      interval            = 30
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout             = 5
+    },
+  ]
 
-# #   instances = ["${aws_instance.web.*.id}"]
+#   instances = ["${aws_instance.web.*.id}"]
 
-#   tags = {
-#       Name = "javaapp"
-#   }
-# }
+  tags = {
+      Name = "javaapp"
+  }
+}
 
 
 
